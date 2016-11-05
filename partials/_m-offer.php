075fac2a37e82ae3b4358600de4ca4843695a432
tfshockness <timoteo.ppt@gmail.com>
2016-11-05 01:30:04 -0700

    offer update
    Updated offer to November offer. Changed the text.

--- partials/_m-offer.php
+++ partials/_m-offer.php
@@ -2,7 +2,7 @@
 	<div class="type-title">
 		<h2 class="title-offer roboto-font"></h2>
 	</div>
-	<p class="roboto-font container clear">30% de descontos em qualquer tratamento. Faça sua avaliação gratuita!</p>
+	<p class="roboto-font container clear">Limpeza de pele + Massagem relaxante. Apenas R$ 100,00</p>
 </div>
 <div class="center">
 	<a href="https://www.facebook.com/fisiostetica/">
